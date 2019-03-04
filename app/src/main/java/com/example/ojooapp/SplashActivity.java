a6028539b0ecfef463ac2dd40452b4d9e471548e
unknown <ostovaar@gmail.com>
2019-03-04 15:10:17 +0330

    Splash Completed,Intent spalshtoMain2

--- app/src/main/java/com/example/ojooapp/SplashActivity.java
+++ app/src/main/java/com/example/ojooapp/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.ojooapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+new Handler().postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        Intent splashtoViewpager = new Intent(SplashActivity.this,MainActivity.class);
+
+        //Do Next: send to ViewpagerActivity
+        startActivity(splashtoViewpager);
+    }
+},3000);
+
+
+
+
     }
 }
