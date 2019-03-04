afd140757120c831d89caac9b91e90ac3b341fd4
unknown <ostovaar@gmail.com>
2019-03-04 22:54:34 +0330

    Toolbar added!

--- app/src/main/java/com/example/ojooapp/ViewPagerActivity.java
+++ app/src/main/java/com/example/ojooapp/ViewPagerActivity.java
@@ -3,12 +3,23 @@ package com.example.ojooapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class ViewPagerActivity extends AppCompatActivity {
+
+    Toolbar toolbarViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+
+        getSupportActionBar().hide();
+
+        toolbarViewPager = findViewById(R.id.toolbarViewPager);
+
+
+
+
     }
 }
